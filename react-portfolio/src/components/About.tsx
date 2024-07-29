bb3d048fc594d0bac0ec73cdcd43ab5fac9b7554
Francis7575 <francis.albert.2009@gmail.com>
2024-07-29 16:11:10 -0700

    updated

--- react-portfolio/src/components/About.tsx
+++ react-portfolio/src/components/About.tsx
@@ -1,25 +1,26 @@
-import image from '/assets/image.png'
 
 const About = () => {
 	return (
-		<section className="px-4">
-			<h2 className="text-[2rem] font-semibold">
+		<section className="px-4 md:px-8">
+			<h2 className="text-[2rem] font-semibold sm:mt-8">
 				About Me
 			</h2>
-			<div className="mt-[1rem] flex flex-col gap-2 max-w-[355px] text-gray">
-				<p>
-					Hey I'm Francis, currently focused on Web Development, aiming to build innovative
-					applications and automate tasks through efficient coding.
-				</p>
-				<p>
-					When I'm not programming, you might find me playing tennis 🎾 with
-					friends if the weather allows.
-				</p>
-				<p>
-					I also have a keen interest in staying active and embrace the joy of running 🏃‍♂️.
-				</p>
+			<div className='sm:flex sm:justify-between sm:items-center sm:gap-4'>
+				<div className="mt-[1rem] flex flex-col gap-2 max-w-[380px] text-gray">
+					<p>
+						Hey I'm Francis, currently focused on Web Development, aiming to build innovative
+						applications and automate tasks through efficient coding.
+					</p>
+					<p>
+						When I'm not programming, you might find me playing tennis 🎾 with
+						friends if the weather allows.
+					</p>
+					<p>
+						I also have a keen interest in staying active and embrace the joy of running 🏃‍♂️.
+					</p>
+				</div>
+				<div className='image-box'></div>
 			</div>
-			<img src={image} alt="Francis's picture" />
 		</section>
 	)
 }
