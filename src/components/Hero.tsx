c63dfa08c29b0a7e817a150e919c1d16c7a559ce
Francis7575 <francis.albert.2009@gmail.com>
2024-08-12 19:21:48 -0700

    updated hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -31,7 +31,7 @@ const Hero = () => {
 				</div>
 			</div>
 			<div className='basis-3/6 flex justify-center pb-[4rem]'>
-				<ModelViewer path={`./assets/test.glb`} scale={[5, 5, 5]} position={[0.2, 0.2, 0.2]} cameraPosition={[0, 0, 6]} />
+				<ModelViewer path={`/assets/test.glb`} scale={[5, 5, 5]} position={[0.2, 0.2, 0.2]} cameraPosition={[0, 0, 6]} />
 			</div>
 		</motion.div>
 	)
