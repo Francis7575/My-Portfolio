6aeca8451a1667f27c56da42bdd16c45256c361b
Francis7575 <francis.albert.2009@gmail.com>
2024-08-14 09:42:41 -0700

    updated

--- src/components/three-js/ModelViewer.tsx
+++ src/components/three-js/ModelViewer.tsx
@@ -1,7 +1,7 @@
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 import Model from './Model';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 type ModelViewerProps = {
   path: string;
@@ -18,15 +18,24 @@ const ModelViewer: React.FC<ModelViewerProps> = ({
 }) => {
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
 
+  useEffect(() => {
+    const handleResize = () => setIsMobile(window.innerWidth <= 768);
+    window.addEventListener('resize', handleResize);
+
+    // Cleanup on unmount
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
-    <div>
-      <Canvas className='canvas' camera={{ position: cameraPosition }}>
+    <div className="canvas-container">
+      <Canvas className="canvas" camera={{ position: cameraPosition }}>
         <ambientLight intensity={4.5} />
         <directionalLight intensity={1.5} />
         <Model path={path} scale={scale} position={position} />
         {!isMobile && (
           <OrbitControls
-            enableZoom={false}  // Disables zooming with the mouse wheel or touch pinch gestures
+            enableZoom={false} // Disables zooming with the mouse wheel or touch pinch gestures
+            enablePan={false} // Disables panning (moving) the camera with drag or touch
             enableRotate={false} // Disables rotation of the camera around the target object
             enableDamping={false} // Disables damping (smooth transition) for the controls
             dampingFactor={0.1} // Sets the damping factor for smoothing the camera movement (not used since damping is disabled)
@@ -34,6 +43,7 @@ const ModelViewer: React.FC<ModelViewerProps> = ({
           />
         )}
       </Canvas>
+      {isMobile && <div className="scroll-overlay"></div>}
     </div>
   );
 };
