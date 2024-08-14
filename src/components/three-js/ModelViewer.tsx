8eef8858fc3295eb235a38e96b770f887e4567ed
Francis7575 <francis.albert.2009@gmail.com>
2024-08-14 09:31:49 -0700

    updated orbitControls

--- src/components/three-js/ModelViewer.tsx
+++ src/components/three-js/ModelViewer.tsx
@@ -1,7 +1,7 @@
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 import Model from './Model';
-
+import { useState } from 'react';
 
 type ModelViewerProps = {
   path: string;
@@ -10,17 +10,31 @@ type ModelViewerProps = {
   cameraPosition?: [number, number, number];
 };
 
-const ModelViewer: React.FC<ModelViewerProps> = ({ path, scale, position = [0, 0, 0], cameraPosition = [0, 2, 5] }) => {
+const ModelViewer: React.FC<ModelViewerProps> = ({
+  path,
+  scale,
+  position = [0, 0, 0],
+  cameraPosition = [0, 2, 5]
+}) => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
   return (
-    <Canvas className='canvas' camera={{ position: cameraPosition }}>
-      <ambientLight intensity={4.5} />
-      <directionalLight intensity={1.5} />
-      <Model path={path} scale={scale} position={position} />
-      <OrbitControls
-        enableZoom={false}
-        enablePan={false} 
-        enableRotate={false} />
-    </Canvas>
+    <div>
+      <Canvas className='canvas' camera={{ position: cameraPosition }}>
+        <ambientLight intensity={4.5} />
+        <directionalLight intensity={1.5} />
+        <Model path={path} scale={scale} position={position} />
+        {!isMobile && (
+          <OrbitControls
+            enableZoom={false}
+            enableRotate={false}
+            enableDamping={false}
+            dampingFactor={0.1}
+            maxPolarAngle={Math.PI / 2} 
+          />
+        )}
+      </Canvas>
+      {isMobile && <div className="scroll-overlay"></div>}
+    </div>
   );
 };
 
