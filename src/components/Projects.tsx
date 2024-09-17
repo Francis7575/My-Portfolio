6196712f72c556fbf235434284b68551615d95ab
Francis7575 <francis.albert.2009@gmail.com>
2024-09-16 23:21:43 -0700

    added carousel for projects components to be display from medium screens and above

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,121 +3,149 @@ import openIcon from "/assets/icon-open.png"
 import { motion, AnimatePresence } from "framer-motion";
 import { ProjectsList } from "../utils/constants";
 import { useState } from "react";
+import { ArrowLeft } from "lucide-react"
+import { ArrowRight } from "lucide-react"
 
 const Projects = () => {
-	const [selectedIndex, setSelectedIndex] = useState<number>(0);
+  const [selectedIndex, setSelectedIndex] = useState<number>(0);
 
-	const selectProject = (index: number) => {
-		setSelectedIndex(index);
-	};
+  const selectProject = (index: number) => {
+    if (index < 0) {
+      setSelectedIndex(ProjectsList.length - 1); // Wrap to the last project
+    } else if (index >= ProjectsList.length) {
+      setSelectedIndex(0); // Wrap to the first project
+    } else {
+      setSelectedIndex(index);
+    }
+  };
 
-	const projectVariants = {
+  const projectVariants = {
     hidden: { opacity: 0, x: 100 }, // animation for exiting
     visible: { opacity: 1, x: 0 },  // animation for entering
     exit: { opacity: 0, x: -100 },  // animation for exiting
   };
 
-	return (
-		<section id="projects">
-			<motion.h2
-				whileInView={{ opacity: 1, y: 0 }}
-				initial={{ opacity: 0, y: -10 }}
-				transition={{ duration: 1.5 }}
-				className="my-10 md:mt-20 text-center text-[1.8rem] font-medium"
-			>
-				Projects
-			</motion.h2>
-			<motion.div className="px-4 md:px-8 xl:px-0 gap-x-7 max-w-[500px] mx-auto"
-				whileInView={{ opacity: 1, x: 0 }}
-				initial={{ opacity: 0, x: 100 }}
-				transition={{ duration: 1.5 }}
-			>
-				<AnimatePresence mode='wait'>
+  return (
+    <section id="projects">
+      <motion.h2
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -10 }}
+        transition={{ duration: 1.5 }}
+        className="my-10 md:mt-20 text-center text-[1.8rem] font-medium"
+      >
+        Projects
+      </motion.h2>
+      <motion.div className="px-4 md:px-8 xl:px-0 gap-x-7 max-w-[500px] mx-auto"
+        whileInView={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: 100 }}
+        transition={{ duration: 1.5 }}
+      >
+        <AnimatePresence mode='wait'>
           {ProjectsList.map((item, idx) =>
             selectedIndex === idx ? (
-              <motion.div
-                key={idx}
-                className="mx-auto w-full mb-8 md:mx-0 flex flex-col max-w-[420px] md:max-w-none"
-                variants={projectVariants}
-                initial="hidden"
-                animate="visible"
-                exit="exit"
-                transition={{ duration: 0.5 }} 
-              >
-                <div className="relative">
-                  <a href={item.liveLink} target="_blank">
-                    <img
-                      className="rounded-t-2xl hover:opacity-50 h-[220px] w-full"
-                      src={item.image}
-                      alt={item.alt}
-                    />
-                  </a>
-                  <a
-                    className="max-w-[25px]"
-                    href={item.repositoryLink}
-                    target="_blank"
-                  >
-                    <img
-                      src={githubIcon}
-                      alt="Github"
-                      className="max-w-[40px] absolute top-2 right-2"
-                    />
-                  </a>
-                  <a href={item.liveLink} target="_blank">
-                    <img
-                      src={openIcon}
-                      alt="Live Web"
-                      className="w-full max-w-[40px] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
-                    />
-                  </a>
-                </div>
-                <div className="px-5 shadow-lg py-4 rounded-b-2xl flex-grow flex flex-col">
-                  <h2 className="text-2xl">{item.name}</h2>
-                  <div className="mt-2">
-                    <p>{item.description}</p>
-                    <ul className="list-disc mt-2 pl-4">
-                      <li>{item.listOne}</li>
-                      <li>{item.listTwo}</li>
-                      {item.listThree && <li>{item.listThree}</li>}
-                      {item.listFour && <li>{item.listFour}</li>}
-                    </ul>
+              <div className="relative">
+                <motion.div
+                  key={idx}
+                  className="mx-auto w-full mb-8 md:mx-0 flex flex-col max-w-[420px] md:max-w-none"
+                  variants={projectVariants}
+                  initial="hidden"
+                  animate="visible"
+                  exit="exit"
+                  transition={{ duration: 0.5 }}
+                >
+                  <div className="relative">
+                    <a href={item.liveLink} target="_blank">
+                      <img
+                        className="rounded-t-2xl hover:opacity-50 h-[220px] w-full"
+                        src={item.image}
+                        alt={item.alt}
+                      />
+                    </a>
+                    <a
+                      className="max-w-[25px]"
+                      href={item.repositoryLink}
+                      target="_blank"
+                    >
+                      <img
+                        src={githubIcon}
+                        alt="Github"
+                        className="max-w-[40px] absolute top-2 right-2"
+                      />
+                    </a>
+                    <a href={item.liveLink} target="_blank">
+                      <img
+                        src={openIcon}
+                        alt="Live Web"
+                        className="w-full max-w-[40px] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+                      />
+                    </a>
                   </div>
-                  <div className="grid-techs">
-                    {item.techs.map((tech, techIdx) => (
-                      <div key={techIdx} className="flex">
-                        <div className="flex items-center gap-2">
-                          <img
-                            src={tech.icon}
-                            alt={tech.name}
-                            className="w-6 h-6"
-                          />
-                          <p className="text-sm">{tech.name}</p>
+                  <div className="px-5 shadow-lg py-4 rounded-b-2xl flex-grow flex flex-col">
+                    <h2 className="text-2xl">{item.name}</h2>
+                    <div className="mt-2">
+                      <p>{item.description}</p>
+                      <ul className="list-disc mt-2 pl-4">
+                        <li>{item.listOne}</li>
+                        <li>{item.listTwo}</li>
+                        {item.listThree && <li>{item.listThree}</li>}
+                        {item.listFour && <li>{item.listFour}</li>}
+                      </ul>
+                    </div>
+                    <div className="grid-techs">
+                      {item.techs.map((tech, techIdx) => (
+                        <div key={techIdx} className="flex">
+                          <div className="flex items-center gap-2">
+                            <img
+                              src={tech.icon}
+                              alt={tech.name}
+                              className="w-6 h-6"
+                            />
+                            <p className="text-sm">{tech.name}</p>
+                          </div>
                         </div>
-                      </div>
-                    ))}
+                      ))}
+                    </div>
                   </div>
+                </motion.div>
+                <div className="hidden md:block">
+                  <button
+                    onClick={() => selectProject(selectedIndex - 1)}
+                    aria-label="Previous Project"
+                    className="size-[40px] absolute left-[-50px] top-[50%] bg-gray-300 rounded-full bg-second-blue hover:opacity-70 text-white flex justify-center items-center"
+                  >
+                    <ArrowLeft />
+                  </button>
+                  <button
+                    onClick={() => selectProject(selectedIndex + 1)}
+                    aria-label="Next Project"
+                    className="size-[40px] bg-second-blue absolute right-[-60px] top-[50%] text-white flex justify-center items-center rounded-full hover:opacity-70"
+
+                  >
+                    <ArrowRight />
+                  </button>
                 </div>
-              </motion.div>
+              </div>
             ) : null
           )}
         </AnimatePresence>
-			</motion.div>
+      </motion.div>
 
-			{/* Render buttons for each project */}
-			<div className="flex justify-center items-center gap-8">
-				{ProjectsList.map((_, idx) => (
-					<button
-						key={idx}
-						aria-pressed={selectedIndex === idx}
-						aria-label={`Tab ${ProjectsList[idx].name}`}
-						className={`size-[10px] md:size-[15px] rounded-full hover:bg-success-two ${selectedIndex === idx ? "bg-success " : "bg-thirdgray hover:opacity-70"
-							} hover:bg-white-50`}
-						onClick={() => selectProject(idx)}
-					/>
-				))}
-			</div>
-		</section >
-	)
+
+      {/* Render buttons for each project */}
+      <div className="flex justify-center items-center gap-8">
+        {ProjectsList.map((_, idx) => (
+          <button
+            key={idx}
+            aria-pressed={selectedIndex === idx}
+            aria-label={`Tab ${ProjectsList[idx].name}`}
+            className={`size-[10px] md:size-[15px] rounded-full hover:bg-success-two ${selectedIndex === idx ? "bg-success " : "bg-thirdgray hover:opacity-70"
+              } hover:bg-white-50`}
+            onClick={() => selectProject(idx)}
+          />
+        ))}
+      </div>
+    </section >
+  )
 }
 
 export default Projects
