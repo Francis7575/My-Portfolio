1e0fbbceae042645c3db6bdf3088fdc0c3ebccc9
Francis7575 <francis.albert.2009@gmail.com>
2024-10-02 13:45:44 -0700

    updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Suspense, lazy, useEffect, useState } from 'react';
+import { Suspense, lazy } from 'react';
 import LoadingPage from './components/LoadingPage'
 
 const Header = lazy(() => import('./components/Header'));
@@ -10,15 +10,9 @@ const Contact = lazy(() => import('./components/contact/Contact'));
 const Footer = lazy(() => import('./components/Footer'));
 
 const App = () => {
-	const [loading, setLoading] = useState(true);
-
-	useEffect(() => {
-		const timer = setTimeout(() => setLoading(false), 200); 
-		return () => clearTimeout(timer);
-	}, []);
 
 	return (
-		<Suspense fallback={loading ? <LoadingPage /> : null}>
+		<Suspense fallback={<LoadingPage />} >
 			<Header />
 			<main className="font-roboto-condensed lg:max-w-[1200px] lg:mx-auto">
 				<Hero />
@@ -28,7 +22,7 @@ const App = () => {
 				<Contact />
 			</main>
 			<Footer />
-		</Suspense>
+		</Suspense >
 	)
 }
 
